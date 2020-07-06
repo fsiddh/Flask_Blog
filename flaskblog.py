f00e5089d4aebc619c159604abaf86aef3e46437
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-06 14:47:39 +0530

    Modified values

--- flaskblog.py
+++ flaskblog.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Falansh Siddh',
+        'author': 'Corey Schafer',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
