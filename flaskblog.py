8f197bbb2186835ff28e255cfa9ffcc709caf88b
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-06 12:18:21 +0530

    Added new files

--- flaskblog.py
+++ flaskblog.py
@@ -1,21 +1,31 @@
-from flask import Flask
-
-# Instantiates flask app
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Falansh Siddh',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
-# 'routes' are what we write on browser to go on different pages
-# '/' and '/home' takes us at same location where home() is loaded
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return '<h1>Hello World</h1>'
+    return render_template('home.html', posts=posts)
 
 
-# When you search 'http://127.0.0.1:5000/about' in browser your about function loads
 @app.route("/about")
 def about():
-    return 'Hello, my name is Falansh Siddh'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
